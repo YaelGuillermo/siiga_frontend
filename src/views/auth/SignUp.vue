8c4e1a72fa04c05ece545abb3c5630c37819b46f
YaelGuillermo <l20211785@tectijuana.edu.mx>
2024-04-06 17:31:41 -0700

    changes in auth

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -1,3 +1,55 @@
+<template>
+    <section>
+      <div>
+        <div class="row d-flex align-items-center justify-content-center h-100">
+          <div class="col-md-8 col-lg-7 col-xl-6">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              class="img-fluid" alt="Phone image">
+          </div>
+          <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <form>
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" id="form1Example13" class="form-control form-control-lg" />
+                <label class="form-label" for="form1Example13">Email address</label>
+              </div>
+  
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <input type="password" id="form1Example23" class="form-control form-control-lg" />
+                <label class="form-label" for="form1Example23">Password</label>
+              </div>
+  
+              <div class="d-flex justify-content-around align-items-center mb-4">
+                <!-- Checkbox -->
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+                  <label class="form-check-label" for="form1Example3"> Remember me </label>
+                </div>
+                <a href="#!">Forgot password?</a>
+              </div>
+  
+              <!-- Submit button -->
+              <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+  
+              <div class="divider d-flex align-items-center my-4">
+                <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+              </div>
+  
+              <!-- Sign in with Google button -->
+              <a class="btn btn-primary btn-lg btn-block" style="background-color: #dd4b39" href="#!"
+                role="button">
+                <i class="fab fa-google me-2"></i>Continue with Google
+              </a>
+  
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+<!--
 <script>
     export default {
         name: 'Signup',
@@ -127,3 +179,4 @@
 </template>
 
 <style src="../styles/auth.css"></style>
+-->
